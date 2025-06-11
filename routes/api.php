4cfb033fb9b1c28baffbfc8022923b705ec9713a
firm <firman.zuh.aff@gmail.com>
2025-06-11 21:50:18 +0700

    kelar api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MenuApiController;
+use App\Http\Controllers\Api\AuthApiController;
 
-Route::get('/test-api', function () {
-    return ['message' => 'API test berhasil!'];
+// Rute publik (tidak perlu login)
+Route::get('/menus', [MenuApiController::class, 'index']);
+Route::get('/menus/{id}', [MenuApiController::class, 'show']);
+
+// Rute yang memerlukan otentikasi (contoh: untuk Penjual)
+Route::middleware('auth:sanctum', 'role:penjual')->group(function () {
+    Route::post('/menus', [MenuApiController::class, 'store']);
+    Route::put('/menus/{id}', [MenuApiController::class, 'update']);
+    Route::delete('/menus/{id}', [MenuApiController::class, 'destroy']);
 });
+
+// Rute untuk otentikasi API
+Route::post('/login', [AuthApiController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthApiController::class, 'logout']);
