caf19c34a0b8585c848db8d7815335845da0259a
firm <firman.zuh.aff@gmail.com>
2025-06-16 14:39:49 +0700

    udah API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('/test-api', function () {
     return ['message' => 'API test berhasil!'];
